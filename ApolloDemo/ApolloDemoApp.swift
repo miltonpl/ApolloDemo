3eb43b168ab2b30581c2d8fd27e2f18b597ba9e2
miltonpl <miltonpalaguachi@gmail.com>
2026-08-01 01:34:29 -0400

    Setup xcode project

---
+++ ApolloDemo/ApolloDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApolloDemoApp.swift
+//  ApolloDemo
+//
+//  Created by Milton Palaguachi on 7/29/26.
+//
+
+import SwiftUI
+
+@main
+struct ApolloDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApolloDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ApolloDemo
+//
+//  Created by Milton Palaguachi on 7/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
